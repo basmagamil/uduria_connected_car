b422d99e3f04dbeb7df20196e7a206cabd412bfd
basmagamil <basma.gamil17@gmail.com>
2020-05-31 21:00:00 +0200

    logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,9 +14,11 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           textTheme: TextTheme(
             body1: TextStyle(fontSize: 36),
-            display1: TextStyle(color: Colors.green, fontSize: 60),
+            display1: TextStyle(
+                color: Colors.green, fontSize: 80, fontFamily: "Digital"),
             body2: TextStyle(fontSize: 28),
-            display2: TextStyle(color: Colors.green, fontSize: 44),
+            display2: TextStyle(
+                color: Colors.green, fontSize: 60, fontFamily: "Digital"),
           )),
       debugShowCheckedModeBanner: false,
       home: MyHomePage(title: 'Speedometer'),
@@ -32,12 +36,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double _lat = 0;
+  double _long = 0;
+  // double _speed = 0; //int
+  int _speed = 0;
+  int _duration1 = 0;
+  int _duration2 = 0;
+  var geolocator = Geolocator();
+  var locationOptions = LocationOptions(accuracy: LocationAccuracy.high);
+  var stopwatch1 = new Stopwatch();
+  var stopwatch2 = new Stopwatch();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  _MyHomePageState() : super() {
+    var timer =
+        new Timer.periodic(new Duration(milliseconds: 1000), _updateTime);
+
+    // Stream<int> stream =
+    //     Stream<int>.periodic(new Duration(milliseconds: 1000), incrementSpeed);
+    // updateSpeed(stream);
+
+    StreamSubscription<Position> positionStream = geolocator
+        .getPositionStream(locationOptions)
+        .listen((Position position) {
+      setState(() {
+        _lat = position?.latitude;
+        _long = position?.longitude;
+        _speed = (position?.speed * 3.6 * 100).round(); //remove 100
+      });
+      if (_speed >= 10 && _speed <= 11 && !stopwatch1.isRunning) {
+        stopwatch1.reset();
+        stopwatch1.start();
+      } else if ((_speed >= 30 || _speed <= 10) && stopwatch1.isRunning) {
+        stopwatch1.stop();
+      } else if (_speed <= 31 && _speed >= 30 && !stopwatch2.isRunning) {
+        stopwatch2.reset();
+        stopwatch2.start();
+      } else if ((_speed <= 10 || _speed >= 30) && stopwatch2.isRunning) {
+        stopwatch2.stop();
+      }
     });
+  }
+
+  // int incrementSpeed(int value) {
+  //   // return value;
+  //   return (40 - value).abs();
+  // }
+
+  // void updateSpeed(Stream<int> stream) async {
+  //   await for (var value in stream) {
+  //     setState(() {
+  //       _speed = value;
+  //     });
+  //   }
+  // }
+
+  _updateTime(Timer timer) {
+    if (stopwatch1.isRunning) {
+      setState(() {
+        _duration1 = (stopwatch1.elapsedMilliseconds * 0.001).round();
+      });
+    }
+    if (stopwatch2.isRunning) {
+      setState(() {
+        _duration2 = (stopwatch2.elapsedMilliseconds * 0.001).round();
+      });
+    }
   }
 
   @override
@@ -60,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     style: Theme.of(context).textTheme.body1,
                   ),
                   Text(
-                    '$_counter',
+                    '$_speed',
                     style: Theme.of(context).textTheme.display1,
                   ),
                   Text(
@@ -80,9 +143,8 @@ class _MyHomePageState extends State<MyHomePage> {
                     style: Theme.of(context).textTheme.body2,
                   ),
                   Text(
-                    '$_counter',
+                    '$_duration1',
                     style: Theme.of(context).textTheme.display2,
-                    // style: Theme.of(context).textTheme.display1,
                   ),
                   Text(
                     'Seconds',
@@ -101,7 +163,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     style: Theme.of(context).textTheme.body2,
                   ),
                   Text(
-                    '$_counter',
+                    '$_duration2',
                     style: Theme.of(context).textTheme.display2,
                   ),
                   Text(
@@ -113,11 +175,6 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
